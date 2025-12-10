1099c4eb42a9da3f0578bcac17595dd77333d0af
maxim010 <140669557+Uhhghhujbh@users.noreply.github.com>
2025-12-10 10:19:34 -0800

    Add Orders route and import in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import Receipt from './pages/Receipt';
 import Tracking from './pages/Tracking';
-import Orders from './pages/Orders'; // <--- NEW IMPORT
+import Orders from './pages/Orders'; 
 
 function App() {
   return (
@@ -19,10 +19,10 @@ function App() {
           <Route path="/shop" element={<Shop />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/orders" element={<Orders />} /> {/* <--- NEW ROUTE */}
+          <Route path="/orders" element={<Orders />} /> 
           
           {/* Protected / Dynamic Routes */}
-          <Route path="/admin" element={<Admin />} />
+          <Route path="/me" element={<Admin />} />
           <Route path="/receipt/:id" element={<Receipt />} />
           <Route path="/track" element={<Tracking />} />
         </Routes>
